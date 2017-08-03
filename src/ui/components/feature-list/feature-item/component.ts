8a026b17b56ca17da5cb7cc356720d447364db15
Joshua Vermeulen <joshua@martletandco.co.nz>
2017-08-03 20:59:08 +1200

    Remove accordion behaviour as not enough content

--- src/ui/components/feature-list/feature-item/component.ts
+++ src/ui/components/feature-list/feature-item/component.ts
@@ -1,23 +1,23 @@
 import Component, { tracked } from '@glimmer/component';
 
 export default class FeatureItem extends Component {
-    @tracked isOpen = false;
+    // @tracked isOpen = true;
 
     @tracked('@feature')
     get feature() {
         return this.args.feature;
     }
 
-    @tracked('isOpen')
-    get stateClass() {
-        if(!this.isOpen) {
-            return 'u-hidden';
-        }
+    // @tracked('isOpen')
+    // get stateClass() {
+    //     if(!this.isOpen) {
+    //         return 'u-hidden';
+    //     }
 
-        return '';
-    }
+    //     return '';
+    // }
 
-    toggle() {
-        this.isOpen = !this.isOpen;
-    }
+    // toggle() {
+    //     this.isOpen = !this.isOpen;
+    // }
 };
