31fb290eb23045f09974c7a01c12832eb16347a8
Sergey Astapov <v-astas-gri@ae.com>
2018-02-08 22:17:58 +0200

    Move QUnit Simplification (aka New Testing API) into separate yaml

--- app/components/roadmap-page/component.js
+++ app/components/roadmap-page/component.js
@@ -2,12 +2,14 @@ import Component from '@ember/component';
 import codeSplitting from './-utils/code-splitting';
 import jsModules from './-utils/js-modules-api';
 import routerService from './-utils/router-service';
+import QUnitSimplification from './-utils/qunit-simplification';
 import moduleUnification from './-utils/module-unification';
 import glimmerCompat from './-utils/glimmer-compat';
 
 const features = [
   jsModules[0],
   routerService[0],
+  QUnitSimplification[0],
   moduleUnification[0],
   glimmerCompat[0],
   codeSplitting[0]
