b40a2f6f37cb66bba6b00e2591e29456749694bf
Robert Wagner <rwwagner90@gmail.com>
2018-02-07 23:21:28 -0500

    Split into separate yaml
    Closes #6

--- app/components/roadmap-page/component.js
+++ app/components/roadmap-page/component.js
@@ -1,6 +1,18 @@
 import Component from '@ember/component';
-import DATA from './-utils/features';
+import codeSplitting from './-utils/code-splitting';
+import jsModules from './-utils/js-modules-api';
+import routerService from './-utils/router-service';
+import moduleUnification from './-utils/module-unification';
+import glimmerCompat from './-utils/glimmer-compat';
+
+const features = [
+  jsModules[0],
+  routerService[0],
+  moduleUnification[0],
+  glimmerCompat[0],
+  codeSplitting[0]
+];
 
 export default Component.extend({
-  features: DATA.features
+  features
 });
