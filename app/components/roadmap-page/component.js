661e1afe7c0c3a7664355174032d0edf229c7e29
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2018-02-12 23:54:42 +0530

    Autogenerate an index of features

--- app/components/roadmap-page/component.js
+++ app/components/roadmap-page/component.js
@@ -1,21 +1,5 @@
 import Component from "@ember/component";
-import codeSplitting from "./-utils/code-splitting";
-import esClasses from "./-utils/es-classes";
-import glimmerCompat from "./-utils/glimmer-compat";
-import jsModules from "./-utils/js-modules-api";
-import moduleUnification from "./-utils/module-unification";
-import QUnitSimplification from "./-utils/qunit-simplification";
-import routerService from "./-utils/router-service";
-
-const features = [
-  jsModules[0],
-  routerService[0],
-  QUnitSimplification[0],
-  moduleUnification[0],
-  glimmerCompat[0],
-  codeSplitting[0],
-  esClasses[0]
-];
+import features from "./-utils/features";
 
 export default Component.extend({
   features
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,32 +1,38 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
+const glob = require("glob");
+const path = require("path");
+const camelCase = require("camelCase");
+const fs = require("fs");
 
 module.exports = function(defaults) {
-
   let app = new EmberApp(defaults, {
     fingerprint: {
-      prepend: 'https://ember-learn.github.io/'
+      prepend: "https://ember-learn.github.io/"
     }
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  const yamlFiles = glob.sync("app/components/roadmap-page/-utils/*.yaml");
+  let features = yamlFiles
+    .map(f => path.basename(f, path.extname(f)))
+    .filter(f => f !== "template");
 
-  app.import('node_modules/markdown-it/dist/markdown-it.js', {
-    using: [
-      { transformation: 'amd', as: 'markdown-it' }
-    ]
+  const featureModule = `
+  ${features.map(f => `import ${camelCase(f)} from "./${f}";`).join("\n")}
+
+  export default [
+    ${features.map(f => `${camelCase(f)}[0],`).join("\n\t")}
+  ];
+  `;
+
+  fs.writeFileSync(
+    "app/components/roadmap-page/-utils/features.js",
+    featureModule
+  );
+
+  app.import("node_modules/markdown-it/dist/markdown-it.js", {
+    using: [{ transformation: "amd", as: "markdown-it" }]
   });
 
   return app.toTree();
