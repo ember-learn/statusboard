d93b941f5421907891985939b4dc5668bc80e5cf
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2018-02-12 22:54:50 +0530

    Add new template to the page

--- app/components/roadmap-page/component.js
+++ app/components/roadmap-page/component.js
@@ -1,10 +1,11 @@
-import Component from '@ember/component';
-import codeSplitting from './-utils/code-splitting';
-import jsModules from './-utils/js-modules-api';
-import routerService from './-utils/router-service';
-import QUnitSimplification from './-utils/qunit-simplification';
-import moduleUnification from './-utils/module-unification';
-import glimmerCompat from './-utils/glimmer-compat';
+import Component from "@ember/component";
+import codeSplitting from "./-utils/code-splitting";
+import esClasses from "./-utils/es-classes";
+import glimmerCompat from "./-utils/glimmer-compat";
+import jsModules from "./-utils/js-modules-api";
+import moduleUnification from "./-utils/module-unification";
+import QUnitSimplification from "./-utils/qunit-simplification";
+import routerService from "./-utils/router-service";
 
 const features = [
   jsModules[0],
@@ -12,7 +13,8 @@ const features = [
   QUnitSimplification[0],
   moduleUnification[0],
   glimmerCompat[0],
-  codeSplitting[0]
+  codeSplitting[0],
+  esClasses[0]
 ];
 
 export default Component.extend({
